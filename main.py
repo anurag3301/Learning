1f7e8809d32ce08d9fdc6bbf7d9de73fc6024d14
Anurag Kumar Singh <anurag3301.0x0@gmail.com>
2025-04-27 15:56:14 +0000

    Added code to check odd or even

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello world")
+def isEven(num):
+    if num % 2 == 0:
+        return True
+    else:
+        return False
+
+num = int(input("Enter a number: "))
+if isEven(num):
+    print("Number you entered is even")
+else:
+    print("Number you entered is odd")
